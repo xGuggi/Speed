92bd76bf70ecb5e72881263432673be2c27a42d1
joshua cox <cjoshpreston@gmail.com>
2024-04-09 19:06:32 -0600

    front end busted

---
+++ frontend/src/App.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function DraggableBox(card_id) {
+  const [isDragging, setIsDragging] = useState(false);
+  const [position, setPosition] = useState({ x: 50, y: 50 });
+
+  const startDrag = (e) => {
+    // // put the data in the transfer pipe
+    // e.dataTransfer.setData("card_id", card_id);
+
+
+    // set the position to the mouse
+    setPosition({
+      x: e.clientX - 50, // Adjust based on element size to center
+      y: e.clientY - 50, // Adjust based on element size to center
+    });
+    // make the movement smooth
+    e.target.style.position = 'absolute';
+    setIsDragging(true);
+  };
+
+  const onDragging = (e) => {
+    if (!isDragging) return;
+    setPosition({
+      x: e.clientX - 50, // Adjust based on element size to center
+      y: e.clientY - 50, // Adjust based on element size to center
+    });
+  };
+
+  const endDrag = () => {
+    setIsDragging(false);
+  };
+
+  return (
+    <div
+      className="Card"
+      style={{
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+      onMouseDown={startDrag}
+      onMouseMove={onDragging}
+      onMouseUp={endDrag}
+      onMouseLeave={endDrag}
+    ></div>
+  );
+}
+
+function App() {
+  const [cards, setCards] = useState([])
+  const [cardData, setCardData] = useState();
+  
+  useEffect(() => {
+    // Update stickman or handle game over logic here
+  }, []);
+
+  function HandleOnDrop(event) {
+    const card_id= event.dataTransfer.getData("card_id");
+    console.log("card_id: ", card_id)
+    setCards([...cards, card_id]);
+  }
+
+  return (
+    
+    <div className="App"> 
+      <div className="Hand">
+        <DraggableBox card_id="aa"/>
+      </div>
+
+      <div className='CardPile' onDrop={HandleOnDrop}>
+        {cards.map((card, index) => (
+          <div className='dropped-card' key={index}>
+              {card}
+          </div>
+        ))}
+      </div>
+
+      <div className="Hand">
+        <DraggableBox card_id="aa"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
