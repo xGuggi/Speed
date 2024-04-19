7ebcf411b07dc0bf23c1e57f945608e36b652233
xGuggi <spencerguggi@gmail.com>
2024-04-18 18:23:27 -0600

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,10 +8,6 @@ import './App.css'
 
 import io from 'socket.io-client';
 
-import { Droppable } from './Droppable';
-import { Draggable } from './Draggable';
-import CardSVG from './CardSVG';
-
 const socket = io('http://localhost:5001', {
   withCredentials: true,
   extraHeaders: {
@@ -25,15 +21,34 @@ export default function App() {
   const [player2Hand, setPlayer2Hand] = useState(["h-5-♠","h-6-♦","h-7-♣","h-8-♥"]);
   const [leftPile, setLeftPile] = useState("l-1-♠");
   const [rightPile, setRightPile] = useState("r-1-♥");
+  const [fullDeck, setFullDeck] = useState([
+    { rank: '2', suit: '♠' },
+    { rank: '3', suit: '♠' },
+    { rank: '4', suit: '♠' },
+    { rank: '5', suit: '♠' },
+    { rank: '6', suit: '♠' },
+    { rank: '7', suit: '♠' },
+    { rank: '8', suit: '♠' },
+    { rank: '9', suit: '♠' },
+    { rank: '10', suit: '♠' },
+    { rank: 'J', suit: '♠' },
+    { rank: 'Q', suit: '♠' },
+    { rank: 'K', suit: '♠' },
+    { rank: 'A', suit: '♠' },
 
-   
-  const handleDraw = (player) => {
-    if (player === 1) {
-      setPlayer1Hand([...player1Hand, "h-1-♠"]);
-    } else {
-      setPlayer2Hand([...player2Hand, "h-1-♠"]);
-    }
-  }
+    { rank: '2', suit: '♦' },
+    { rank: '3', suit: '♦' },
+    { rank: '4', suit: '♦' },
+    { rank: '5', suit: '♦' },
+    { rank: '6', suit: '♦' },
+    { rank: '7', suit: '♦' },
+    { rank: '8', suit: '♦' },
+    { rank: '9', suit: '♦' },
+    { rank: '10', suit: '♦' },
+    { rank: 'J', suit: '♦' },
+    { rank: 'Q', suit: '♦' },
+    { rank: 'K', suit: '♦' },
+    { rank: 'A', suit: '♦' },
 
     { rank: '2', suit: '♣' },
     { rank: '3', suit: '♣' },
@@ -64,197 +79,18 @@ export default function App() {
     { rank: 'A', suit: '♥' },
   ]);
 
-  let shuffledDeck = [];
-  let leftDisregard = [];
-  let rightDisregard = [];
-  let playerOneStash = [];
-  let playerTwoStash = [];
-  let playerOneHand = [];
-  let playerTwoHand = [];
-  let leftDeck = [];
-  let rightDeck = [];
-  ///////////////////logic////////////////////////
-  function checkIfNone(event) {
-    let playerOneRank;
-    let playerTwoRank;
-    let playable = false; 
-    leftDisRank = parseInt(leftDisregard[0].rank);
-    rightDisRank = parseInt(rightDisregard[0].rank);
-    for (let i = 0; i < playerOneHand.length; i++)
-    {
-      switch(playerOneHand[i].rank) 
-      {
-        case 'A':
-          playerOneRank = 14;
-          break;
-        case 'K':
-          playerOneRank = 13;
-          break;
-        case 'Q':
-          playerOneRank = 12;
-          break;
-        case 'J':
-          playerOneRank = 11;
-          break;
-        default:
-          playerOneRank = parseInt(playerOneHand[i].rank);
-          break;
-      }
-      if ((playerOneRank === 14 && leftDisRank === 2) ||(playerOneRank === 14 && rightDisRank === 2))
-      {
-        playable = true;
-      }
-      else if ((playerOneRank === leftDisRank - 1) || (playerOneRank === leftDisRank + 1) || (playerOneRank === rightDisRank + 1) || (playerOneRank === rightDisRank - 1))
-      {
-        playable = true;
-      }
-    }
-    for (let i = 0; i < playerTwoHand.length; i++)
-    {
-      switch(playerTwoHand[i].rank) 
-      {
-        case 'A':
-          playerTwoRank = 14;
-          break;
-        case 'K':
-          playerTwoRank = 13;
-          break;
-        case 'Q':
-          playerTwoRank = 12;
-          break;
-        case 'J':
-          playerTwoRank = 11;
-          break;
-        default:
-          playerTwoRank = parseInt(playerTwoHand[i].rank);
-          break;
-      }
-      if ((playerTwoRank === 14 && leftDisRank === 2) ||(playerTwoRank === 14 && rightDisRank === 2))
-      {
-        playable = true;
-      }
-      else if ((playerTwoRank === leftDisRank - 1) || (playerTwoRank === leftDisRank + 1) || (playerTwoRank === rightDisRank + 1) || (playerTwoRank === rightDisRank - 1))
-      {
-        playable = true;
-      }
-    }
-    return playable; 
-  }
-
-
-
-  function popForOne(event){
-    if (!playerOneStash.length)
-    {
-      return;
-    }
-    else if (playerOneHand.length < 4)
-    {
-      playerOneHand.push(playerOneStash[0]);
-      playerOneStash.shift();
+  const handleDraw = (player) => {
+    if (player === 1) {
+      setPlayer1Hand([...player1Hand, "h-1-♠"]);
+    } else {
+      setPlayer2Hand([...player2Hand, "h-1-♠"]);
     }
   }
 
-  function popForTwo(event){
-    if (!playerTwoStash.length)
-    {
-      return;
-    }
-    else if (playerTwoHand.length < 4)
-    {
-      playerTwoHand.push(playerTwoStash[0]);
-      playerTwoStash.shift();
-    }
-  }
-
-  function populateOneHand(event){
-    z = 0;
-    for (let i = 0; i < playerOneStash.length; i++)
-    {
-      if (z > 4)
-      {
-        continue;
-      }
-      playerOneHand[i] = playerOneStash[i];
-      playerOneStash.shift();
-    }
-  }
-
-  function populateTwoHand(event){
-    z = 0;
-    for (let i = 0; i < playerTwoStash.length; i++)
-    {
-      if (z > 4)
-      {
-        continue;
-      }
-      playerTwoHand[i] = playerTwoStash[i];
-      playerTwoStash.shift();
-    }
-  }
-
-  ////////////////////////////////////////////////
-
-
-  //////////////////functions for seperating cards////////////////////////
-  function playerOneDeck(event) {
-    playerOneStash = [];
-    for (let i = 0; i < 20; i++)
-    {
-      playerOneStash[i] = event[i];
-    }
-    console.log(playerOneStash);
-  }
-  
-  function playerTwoDeck(event) {
-    playerOneStash = [];
-    let z = 0;
-    for (let i = 20; i < 40; i++)
-    {
-      if (i > 19)
-      {
-        playerTwoStash[z] = event[i];
-        z++;
-      }
-    }
-    console.log(playerTwoStash);
-  }
-  
-  function leftCards(event) {
-    leftDeck = [];
-    let z = 0;
-    for (let i = 40; i < 46; i++)
-    {
-      if (i > 39)
-      {
-        leftDeck[z] = event[i];
-        z++;
-      }
-    }
-    console.log(leftDeck);
-  }
-  
-  function rightCards(event) {
-    leftDeck = [];
-    let z = 0; 
-    for (let i = 46; i < 52; i++)
-    {
-      if (i > 45) 
-      {
-        rightDeck[z] = event[i];
-        z++;
-      }
-    }
-    console.log(rightDeck);
-  }
-  ///////////////////////////////////////////////////
-
-
-
-
-
-  const handleDraw = ()=> {
-    setHand([...hand, "h-" + fullDeck[15].rank + "-" + fullDeck[15].suit]);
+  const handleStalemate = ()=> {
+    // GET NEW CARDS BEFORE SETTING
+    setLeftPile("l-" + "F" + "-" + "F");
+    setRightPile("r-" + "F" + "-" + "F");
   }
 
   useEffect(() => {
@@ -266,24 +102,9 @@ export default function App() {
     console.log("insideUseEffect");
   }, []);
 
-  socket.on('cards', (shuffledArray) =>{
-    //set shuffled arrray
-    shuffledDeck = shuffledArray;
-    //Set up game board
-    playerOneDeck(shuffledArray);
-    playerTwoDeck(shuffledArray);
-    leftCards(shuffledArray);
-    rightCards(shuffledArray);
-  });
-
-
-  useEffect(() => {
-    socket.on('id', (id) => {
-      setName(id);
-      console.log(id); 
-    });
-    socket.emit('staleMate', fullDeck);
-  }, []);
+  socket.on('cards', (shuffledDeck) => {
+    setFullDeck(shuffledDeck);
+  })
 
 
     function handleDragEnd(event) {
@@ -304,6 +125,7 @@ export default function App() {
     } else if (event.over.id.split('-')[0] === "p2") {
       updatedPlayer2Hand = updatedPlayer2Hand.filter(cardid => cardid !== cardID);
     }
+
     if (over.id.split('-')[0] === "l") {
       console.log(over.id.split('-')[1]);
       console.log(rank);
